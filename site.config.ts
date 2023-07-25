46b4861f82f5a52ad9871f8853c164bdd088fdd5
Atai <20579426+atai-doul@users.noreply.github.com>
2023-07-26 01:05:22 +0800

    Update site.config.ts
    显示异常恢复默认设置

--- site.config.ts
+++ site.config.ts
@@ -1,25 +1,26 @@
+// 5d0f458d9a1b42129811aeb92c9fb642
 import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '5d0f458d9a1b42129811aeb92c9fb642',
+  rootNotionPageId: '7875426197cf461698809def95960ebf',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: '西狂部落阁',
-  domain: 'xikuang.ren',
-  author: '西狂',
+  name: 'Next.js Notion Starter Kit',
+  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  author: 'Travis Fischer',
 
   // open graph metadata (optional)
-  description: '点滴珍藏 ｜ 快乐分享',
+  description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  // twitter: '',
-  // github: 'atai-doul',
-  // linkedin: '',
+  twitter: 'transitive_bs',
+  github: 'transitive-bullshit',
+  linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
