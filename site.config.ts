238bab21e1e50f151b13cfd90b70f6f880691d49
xi-kuang <20579426+xi-kuang@users.noreply.github.com>
2023-07-26 03:03:06 +0800

    Update site.config.ts
    更改GitHub用户名

--- site.config.ts
+++ site.config.ts
@@ -20,7 +20,7 @@ export default siteConfig({
 
   // social usernames (optional)
   // twitter: 'transitive_bs',
-  github: 'atai-doul',
+  github: 'xi-kuang',
   // linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
