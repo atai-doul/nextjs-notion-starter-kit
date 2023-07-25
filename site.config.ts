1a2dacf3232cca2becafc5ac3b4882510eff0673
Atai <20579426+atai-doul@users.noreply.github.com>
2023-07-26 01:09:42 +0800

    Update site.config.ts
    update: notion page id

--- site.config.ts
+++ site.config.ts
@@ -1,9 +1,10 @@
-// 5d0f458d9a1b42129811aeb92c9fb642
+// 默认：7875426197cf461698809def95960ebf
+// 我的：5d0f458d9a1b42129811aeb92c9fb642
 import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '5d0f458d9a1b42129811aeb92c9fb642',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
